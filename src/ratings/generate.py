3516d2ee059428ba5b5720c11442b98e75e17ee0
soulFood5632 <loganu@shaw.ca>
2024-11-30 11:18:38 -0800

    more formatting updates

--- src/ratings/generate.py
+++ src/ratings/generate.py
@@ -26,4 +26,39 @@ def generate_faceoff():
 
 
 def generate_defence():
-    pass
+    hr_data = db.load_hr(["14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23"])
+    print(f"Collected {len(hr_data)} entries")
+
+    relevant_table = hr_data[
+        [
+            "Player",
+            "season",
+            "Pos",
+            "GP",
+            "ATOI",
+            "BLK",
+            "CorsiForPerc",
+            "FenwickPerc",
+            "CorsiForPercRel",
+            'FenwickPercRel',
+            'CorsiAgainst',
+            'DZoneStartPerc',
+            'EPlusMinus',
+            'GAPer60',
+            'XGA',
+            'XGF',
+            'GFPer60'
+        ]
+    ]
+
+    relevant_table["ATOI"] = relevant_table.apply(lambda x: convert_time(x["ATOI"]), axis=1)
+    relevant_table['exGA_60'] = relevant_table['XGA'] / relevant_table['GP'] / relevant_table['ATOI'] * 3600
+    relevant_table['exGF60'] = relevant_table['XGF'] / relevant_table['GP'] / relevant_table['ATOI'] * 3600
+    relevant_table['BLK_60'] = relevant_table['BLK'] / relevant_table['GP'] / relevant_table['ATOI'] * 3600
+    relevant_table['CorsiAgainst_60'] = relevant_table['CorsiAgainst'] / relevant_table['GP'] / relevant_table[
+        'ATOI'] * 3600
+
+
+def convert_time(atoi: str):
+    times = atoi.split(":")
+    return int(times[0]) * 60 + int(times[1])
